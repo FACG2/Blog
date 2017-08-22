ff1713fc1c678ee1aad8957dfd9bdf22742fc72c
samer elaila <samer-elaila@live.com>
2017-08-22 13:23:51 +0300

    add getData  and postData

--- src/handlers/post.js
+++ src/handlers/post.js
@@ -1,3 +1,22 @@
-module.exports = {
+function handleSignup (req, res) {
 
+}
+
+function handleAddBlog (req, res) {
+
+}
+
+function handleEditBlog (req, res) {
+
+}
+
+function handleDeleteBlog (req, res) {
+
+}
+
+module.exports = {
+  handleSignup,
+  handleAddBlog,
+  handleEditBlog,
+  handleDeleteBlog
 };
